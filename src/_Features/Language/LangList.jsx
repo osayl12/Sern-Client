ae78c4444ac53a314045e60a52b764d674ecae1b
osayl12 <hamedosayl@gmail.com>
2026-05-05 19:37:10 +0300

    שימוש בהוק כדי להביא את המידע להצגה בטבלה

--- src/_Features/Language/LangList.jsx
+++ src/_Features/Language/LangList.jsx
@@ -1,36 +1,49 @@
-import React from 'react';
-import {Table, TableBody, TableCell, TableHead, TableRow, Typography} from "@mui/material";
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+
+import { useGetLangs } from "./apiHookLang.js";
 
 function LangList() {
-    const data = [
-        {id:1, name:"English"},
-        {id:2, name:"Arabic"},
-        {id:3, name:"French"},
-        {id:4, name:"Spanish"},
-    ]
-    return (
-        <>
-            <Typography variant="h2" sx={{ textAlign: 'center', mt: 2 }}>
-                שפות
-            </Typography>
-            <Table>
-                <TableHead>
-                <TableRow>
-                    <TableCell>מזהה</TableCell>
-                    <TableCell>שם</TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {data.map((item)=>(
-                    <TableRow key={item.id}>
-                        <TableCell>{item.id}</TableCell>
-                        <TableCell>{item.name}</TableCell>
-                    </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-        </>
-    );
+  const {
+    data: { list: LangArr } = {},
+    isLoading,
+    isError,
+    error,
+  } = useGetLangs();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+ 
+  return (
+    <>
+      <Typography variant="h2" sx={{ textAlign: "center", mt: 2 }}>
+        שפות
+      </Typography>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>מזהה</TableCell>
+            <TableCell>שם</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {LangArr?.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
 }
 
 export default LangList;
