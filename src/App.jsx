7d146b2b9509d0aaea4e2f6cec6b46fafb718512
osayl12 <hamedosayl@gmail.com>
2026-05-05 17:42:30 +0300

    add query to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-//npm install react-router  @mui/material @emotion/react @emotion/styled @mui/icons-material   stylis-plugin-rtl @emotion/cache material-ui-confirm  @tanstack/react-query 
-import './App.css'
+//npm install react-router  @mui/material @emotion/react @emotion/styled @mui/icons-material   stylis-plugin-rtl @emotion/cache material-ui-confirm  @tanstack/react-query
+import "./App.css";
 
-import { RouterProvider} from 'react-router';
+import { RouterProvider } from "react-router";
 import router from "./Routers/main_R";
 
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-import rtlPlugin from 'stylis-plugin-rtl';
-import { prefixer } from 'stylis';
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
 
-import { ThemeProvider } from '@mui/material';
-import { CssBaseline } from '@mui/material';
-import { theme } from './theme_params.jsx';
+import { ThemeProvider } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { theme } from "./theme_params.jsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 // Create rtl cache
 const cacheRtl = createCache({
-    key: 'muirtl',
-    stylisPlugins: [prefixer, rtlPlugin],
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
 });
 
-
 function App() {
-
   return (
-    <CacheProvider value={cacheRtl}>
+    <QueryClientProvider client={queryClient}>
+      <CacheProvider value={cacheRtl}>
         <ThemeProvider theme={theme}>
-            <CssBaseline /> {/* This normalizes the styles */}
-            <RouterProvider router={router} />
+          <CssBaseline /> {/* This normalizes the styles */}
+          <RouterProvider router={router} />
         </ThemeProvider>
-    </CacheProvider>
-  )
+      </CacheProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
